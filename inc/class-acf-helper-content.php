2c6aa8fe3ae9942db90349526c817e86db27549f
Piotr Kierzniewski <p.kierzniewski@viewone.pl>
2017-07-20 21:08:11 +0200

    Add file field

--- inc/class-acf-helper-content.php
+++ inc/class-acf-helper-content.php
@@ -209,4 +209,58 @@ class Acf_Helper_Content {
 
 		return $field;
 	}
+
+	/**
+	 * Get acf file field
+	 *
+	 * Helper function for retrieving acf file field.
+	 *
+	 * @param string $name Field name used as key and name.
+	 * @param string $label Field label displayed at the admin.
+	 * @param array  $args Additional arguments which extend defaults.
+	 *
+	 * @return array Acf file field
+	 *
+	 * @throws \InvalidArgumentException Throw exception if $name or $label are
+	 * 									 not string.
+	 */
+	public function get_acf_file_field( $name, $label, $args = array() ) {
+
+		$field_defaults = array(
+			'key' => '',
+			'label' => '',
+			'name' => '',
+			'type' => 'file',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'array',
+			'library' => 'all',
+			'min_size' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		);
+
+		$field = wp_parse_args( $args, $field_defaults );
+
+		if ( ! is_string( $name ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $name parameter is not a type of string but "%s".', gettype( $name ) ) );
+		}
+
+		$field['key']  = 'field_' . $name;
+		$field['name'] = $name;
+
+		if ( ! is_string( $label ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $label parameter is not a type of string but "%s".', gettype( $label ) ) );
+		}
+
+		$field['label'] = $label;
+
+		return $field;
+	}
 }
--- tests/class-acf-helper-content-test.php
+++ tests/class-acf-helper-content-test.php
@@ -387,4 +387,128 @@ class Acf_Helper_Content_Test extends WP_UnitTestCase {
 
 		$plugin['acf-helper/content']->get_acf_image_field( 'bar', 1 );
 	}
+
+	/**
+	 * Testing get_acf_file_field
+	 */
+
+	/**
+	 * Test if getting acf file field return expected results
+	 */
+	function test_get_acf_file_field() {
+
+		$plugin = acf_helper();
+
+		$expected = array(
+			'key' => 'field_foo',
+			'label' => 'Foo',
+			'name' => 'foo',
+			'type' => 'file',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'array',
+			'library' => 'all',
+			'min_size' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		);
+
+		$expected_filled = array(
+			'key' => 'field_bar',
+			'label' => 'Bar',
+			'name' => 'bar',
+			'type' => 'file',
+			'instructions' => 'Upload some file',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'file',
+				'id' => 'bar_file',
+			),
+			'return_format' => 'id',
+			'library' => 'all',
+			'min_size' => '1',
+			'max_size' => '10',
+			'mime_types' => 'application/pdf',
+		);
+
+		$field 		  = $plugin['acf-helper/content']->get_acf_file_field( 'foo', 'Foo' );
+		$field_filled = $plugin['acf-helper/content']->get_acf_file_field( 'bar', 'Bar', array(
+			'instructions' => 'Upload some file',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'file',
+				'id' => 'bar_file',
+			),
+			'return_format' => 'id',
+			'library' => 'all',
+			'min_size' => '1',
+			'max_size' => '10',
+			'mime_types' => 'application/pdf',
+		) );
+
+		$this->assertEquals( $field, $expected );
+		$this->assertEquals( $field_filled, $expected_filled );
+	}
+
+	/**
+	 * Test if getting acf file with null as $name value throw expected exception
+	 */
+	function test_get_acf_file_field_throwing_exception_when_name_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/content']->get_acf_file_field( null, 'Foo' );
+	}
+
+	/**
+	 * Test if getting acf file with integer as $name value throw expected exception
+	 */
+	function test_get_acf_file_field_throwing_exception_when_name_is_integer() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "integer".' );
+
+		$plugin['acf-helper/content']->get_acf_file_field( 1, 'Foo' );
+	}
+
+	/**
+	 * Test if getting acf file with null as $label value throw expected exception
+	 */
+	function test_get_acf_file_field_throwing_exception_when_label_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/content']->get_acf_file_field( 'bar', null );
+	}
+
+	/**
+	 * Test if getting acf file with integer as $label value throw expected exception
+	 */
+	function test_get_acf_file_field_throwing_exception_when_label_is_integer() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "integer".' );
+
+		$plugin['acf-helper/content']->get_acf_file_field( 'bar', 1 );
+	}
 }
