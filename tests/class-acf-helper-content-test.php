f1fc6feb653d999aeb53c15b6bebaf0e0ac46957
Piotr Kierzniewski <p.kierzniewski@viewone.pl>
2017-07-20 20:53:58 +0200

    Add oembed field

--- tests/class-acf-helper-content-test.php
+++ tests/class-acf-helper-content-test.php
@@ -133,4 +133,119 @@ class Acf_Helper_Content_Test extends WP_UnitTestCase {
 
 		$plugin['acf-helper/content']->get_acf_wysiwyg_field( 'bar', 1.10 );
 	}
+
+	/**
+	 * Testing get_acf_oembed_field
+	 */
+
+	/**
+	 * Test if getting acf oembed field return expected results
+	 */
+	function test_get_acf_oembed_field() {
+
+		$plugin = acf_helper();
+
+		$expected = array(
+			'key' => 'field_foo',
+			'label' => 'Foo',
+			'name' => 'foo',
+			'type' => 'oembed',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'width' => '',
+			'height' => '',
+		);
+
+		$expected_filled = array(
+			'key' => 'field_bar',
+			'label' => 'Bar',
+			'name' => 'bar',
+			'type' => 'oembed',
+			'instructions' => 'Type some url',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'oembed',
+				'id' => 'bar_oembed',
+			),
+			'width' => '640',
+			'height' => '480',
+		);
+
+		$field 		  = $plugin['acf-helper/content']->get_acf_oembed_field( 'foo', 'Foo' );
+		$field_filled = $plugin['acf-helper/content']->get_acf_oembed_field( 'bar', 'Bar', array(
+			'instructions' => 'Type some url',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'oembed',
+				'id' => 'bar_oembed',
+			),
+			'width' => '640',
+			'height' => '480',
+		) );
+
+		$this->assertEquals( $field, $expected );
+		$this->assertEquals( $field_filled, $expected_filled );
+	}
+
+	/**
+	 * Test if getting acf oembed with null as $name value throw expected exception
+	 */
+	function test_get_acf_oembed_field_throwing_exception_when_name_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/content']->get_acf_oembed_field( null, 'Foo' );
+	}
+
+	/**
+	 * Test if getting acf oembed with integer as $name value throw expected exception
+	 */
+	function test_get_acf_oembed_field_throwing_exception_when_name_is_integer() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "integer".' );
+
+		$plugin['acf-helper/content']->get_acf_oembed_field( 1, 'Foo' );
+	}
+
+	/**
+	 * Test if getting acf oembed with null as $label value throw expected exception
+	 */
+	function test_get_acf_oembed_field_throwing_exception_when_label_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/content']->get_acf_oembed_field( 'bar', null );
+	}
+
+	/**
+	 * Test if getting acf oembed with integer as $label value throw expected exception
+	 */
+	function test_get_acf_oembed_field_throwing_exception_when_label_is_integer() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "integer".' );
+
+		$plugin['acf-helper/content']->get_acf_oembed_field( 'bar', 1 );
+	}
 }
