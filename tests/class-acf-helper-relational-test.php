fe742dc274c2f6e3ba8b9334bdc6e4a4fae026d2
Piotr Kierzniewski <p.kierzniewski@viewone.pl>
2017-08-04 18:27:11 +0200

    Add user field

--- tests/class-acf-helper-relational-test.php
+++ tests/class-acf-helper-relational-test.php
@@ -559,4 +559,122 @@ class Acf_Helper_Relational_Test extends WP_UnitTestCase {
 
 		$plugin['acf-helper/relational']->get_acf_taxonomy_field( 'bar', 1.10 );
 	}
+
+	/**
+	 * Testing get_acf_user_field
+	 */
+
+	/**
+	 * Test if getting acf user field return expected results
+	 */
+	function test_get_acf_user_field() {
+
+		$plugin = acf_helper();
+
+		$expected = array(
+			'key' => 'field_foo',
+			'label' => 'Foo',
+			'name' => 'foo',
+			'type' => 'user',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'role' => '',
+			'allow_null' => 0,
+			'multiple' => 0,
+		);
+
+		$expected_filled = array(
+			'key' => 'field_bar',
+			'label' => 'Bar',
+			'name' => 'bar',
+			'type' => 'user',
+			'instructions' => 'Choose any user',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'user',
+				'id' => 'bar_user',
+			),
+			'role' => 'administrator',
+			'allow_null' => 1,
+			'multiple' => 1,
+		);
+
+		$field 		  = $plugin['acf-helper/relational']->get_acf_user_field( 'foo', 'Foo' );
+		$field_filled = $plugin['acf-helper/relational']->get_acf_user_field( 'bar', 'Bar', array(
+			'instructions' => 'Choose any user',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'user',
+				'id' => 'bar_user',
+			),
+			'role' => 'administrator',
+			'allow_null' => 1,
+			'multiple' => 1,
+		) );
+
+		$this->assertEquals( $field, $expected );
+		$this->assertEquals( $field_filled, $expected_filled );
+	}
+
+	/**
+	 * Test if getting acf user with null as $name value throw expected exception
+	 */
+	function test_get_acf_user_field_throwing_exception_when_name_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/relational']->get_acf_user_field( null, 'Foo' );
+	}
+
+	/**
+	 * Test if getting acf user with integer as $name value throw expected exception
+	 */
+	function test_get_acf_user_field_throwing_exception_when_name_is_integer() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "integer".' );
+
+		$plugin['acf-helper/relational']->get_acf_user_field( 1, 'Foo' );
+	}
+
+	/**
+	 * Test if getting acf user with null as $label value throw expected exception
+	 */
+	function test_get_acf_user_field_throwing_exception_when_label_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/relational']->get_acf_user_field( 'bar', null );
+	}
+
+	/**
+	 * Test if getting acf user with integer as $label value throw expected exception
+	 */
+	function test_get_acf_user_field_throwing_exception_when_label_is_integer() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "double".' );
+
+		$plugin['acf-helper/relational']->get_acf_user_field( 'bar', 1.10 );
+	}
 }
