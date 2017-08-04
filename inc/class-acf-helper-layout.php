cb65d4d1c3d918c0a623a74e3b65586bf223ce74
Piotr Kierzniewski <p.kierzniewski@viewone.pl>
2017-08-04 22:31:26 +0200

    Add flexible content field

--- inc/class-acf-helper-layout.php
+++ inc/class-acf-helper-layout.php
@@ -216,4 +216,121 @@ class Acf_Helper_Layout {
 
 		return $field;
 	}
+
+	/**
+	 * Get acf flexible content field
+	 *
+	 * Helper function for retrieving acf flexible content field.
+	 *
+	 * @param string $name Field name used as key and name.
+	 * @param string $label Field label displayed at the admin.
+	 * @param array  $layouts Repeatable field sub fields.
+	 * @param array  $args Additional arguments which extend defaults.
+	 *
+	 * @return array Acf flexible content field
+	 *
+	 * @throws \InvalidArgumentException Throw exception if $name or $label are
+	 * 									 not string.
+	 */
+	public function get_acf_flexible_content_field( $name, $label, $layouts, $args = array() ) {
+
+		$field_defaults = array(
+			'key' => '',
+			'label' => '',
+			'name' => '',
+			'type' => 'flexible_content',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'layouts' => array(),
+			'button_label' => 'Add Row',
+			'min' => '',
+			'max' => '',
+		);
+
+		$field = wp_parse_args( $args, $field_defaults );
+
+		if ( ! is_string( $name ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $name parameter is not a type of string but "%s".', gettype( $name ) ) );
+		}
+
+		$field['key']  = 'field_' . $name;
+		$field['name'] = $name;
+
+		if ( ! is_string( $label ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $label parameter is not a type of string but "%s".', gettype( $label ) ) );
+		}
+
+		$field['label'] = $label;
+
+		if ( ! is_array( $layouts ) || empty( $layouts ) ) {
+			throw new \InvalidArgumentException( 'It looks like $layouts parameter is not a type of array or is empty.' );
+		}
+
+		$field['layouts'] = $layouts;
+
+		return $field;
+	}
+
+	/**
+	 * Get acf flexible content layout
+	 *
+	 * Helper function for retrieving acf flexible content layout.
+	 *
+	 * @param string $name Field name used as key and name.
+	 * @param string $label Field label displayed at the admin.
+	 * @param array  $sub_fields Repeatable field sub fields.
+	 * @param array  $args Additional arguments which extend defaults.
+	 *
+	 * @return array Acf flexible content layout
+	 *
+	 * @throws \InvalidArgumentException Throw exception if $name or $label are
+	 * 									 not string.
+	 */
+	public function get_acf_flexible_content_layout( $name, $label, $sub_fields, $args = array() ) {
+
+		$field_defaults = array(
+			'key' => '',
+			'name' => '',
+			'label' => '',
+			'display' => 'block',
+			'sub_fields' => array(),
+			'min' => '',
+			'max' => '',
+		);
+
+		$field = wp_parse_args( $args, $field_defaults );
+
+		if ( ! is_string( $name ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $name parameter is not a type of string but "%s".', gettype( $name ) ) );
+		}
+
+		$field['key']  = 'layout_' . $name;
+		$field['name'] = $name;
+
+		if ( ! is_string( $label ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $label parameter is not a type of string but "%s".', gettype( $label ) ) );
+		}
+
+		$field['label'] = $label;
+
+		if ( ! is_array( $sub_fields ) || empty( $sub_fields ) ) {
+			throw new \InvalidArgumentException( 'It looks like $sub_fields parameter is not a type of array or is empty.' );
+		}
+
+		foreach ( $sub_fields as $sub_field_key => $sub_field_value ) {
+
+			$sub_fields[ $sub_field_key ]['key'] = sprintf( 'field_%s_%s', $name, $sub_fields[ $sub_field_key ]['name'] );
+			$sub_fields[ $sub_field_key ]['name'] = sprintf( '%s_%s', $name, $sub_fields[ $sub_field_key ]['name'] );
+		}
+
+		$field['sub_fields'] = $sub_fields;
+
+		return $field;
+	}
 }
