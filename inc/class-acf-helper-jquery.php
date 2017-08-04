8769cfbc5acac9eae4ffb71d2a041cbf227a3ef0
Piotr Kierzniewski <p.kierzniewski@viewone.pl>
2017-08-04 19:01:01 +0200

    Add time picker field

--- inc/class-acf-helper-jquery.php
+++ inc/class-acf-helper-jquery.php
@@ -202,4 +202,55 @@ class Acf_Helper_JQuery {
 
 		return $field;
 	}
+
+	/**
+	 * Get acf time picker field
+	 *
+	 * Helper function for retrieving acf time picker field.
+	 *
+	 * @param string $name Field name used as key and name.
+	 * @param string $label Field label displayed at the admin.
+	 * @param array  $args Additional arguments which extend defaults.
+	 *
+	 * @return array Acf time picker field
+	 *
+	 * @throws \InvalidArgumentException Throw exception if $name or $label are
+	 * 									 not string.
+	 */
+	public function get_acf_time_picker_field( $name, $label, $args = array() ) {
+
+		$field_defaults = array(
+			'key' => '',
+			'label' => '',
+			'name' => '',
+			'type' => 'time_picker',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'display_format' => 'g:i a',
+			'return_format' => 'g:i a',
+		);
+
+		$field = wp_parse_args( $args, $field_defaults );
+
+		if ( ! is_string( $name ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $name parameter is not a type of string but "%s".', gettype( $name ) ) );
+		}
+
+		$field['key']  = 'field_' . $name;
+		$field['name'] = $name;
+
+		if ( ! is_string( $label ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $label parameter is not a type of string but "%s".', gettype( $label ) ) );
+		}
+
+		$field['label'] = $label;
+
+		return $field;
+	}
 }
--- tests/class-acf-helper-jquery-test.php
+++ tests/class-acf-helper-jquery-test.php
@@ -366,4 +366,119 @@ class Acf_Helper_JQuery_Test extends WP_UnitTestCase {
 
 		$plugin['acf-helper/jquery']->get_acf_date_time_picker_field( 'bar', 1.10 );
 	}
+
+	/**
+	 * Testing get_acf_time_picker_field
+	 */
+
+	/**
+	 * Test if getting acf date time picker field return expected results
+	 */
+	function test_get_acf_time_picker_field() {
+
+		$plugin = acf_helper();
+
+		$expected = array(
+			'key' => 'field_foo',
+			'label' => 'Foo',
+			'name' => 'foo',
+			'type' => 'time_picker',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'display_format' => 'g:i a',
+			'return_format' => 'g:i a',
+		);
+
+		$expected_filled = array(
+			'key' => 'field_bar',
+			'label' => 'Bar',
+			'name' => 'bar',
+			'type' => 'time_picker',
+			'instructions' => 'Choose time',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'time_picker',
+				'id' => 'bar_time_picker',
+			),
+			'display_format' => 'H:i:s',
+			'return_format' => 'H:i:s',
+		);
+
+		$field 		  = $plugin['acf-helper/jquery']->get_acf_time_picker_field( 'foo', 'Foo' );
+		$field_filled = $plugin['acf-helper/jquery']->get_acf_time_picker_field( 'bar', 'Bar', array(
+			'instructions' => 'Choose time',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'time_picker',
+				'id' => 'bar_time_picker',
+			),
+			'display_format' => 'H:i:s',
+			'return_format' => 'H:i:s',
+		) );
+
+		$this->assertEquals( $field, $expected );
+		$this->assertEquals( $field_filled, $expected_filled );
+	}
+
+	/**
+	 * Test if getting acf date time picker with null as $name value throw expected exception
+	 */
+	function test_get_acf_time_picker_field_throwing_exception_when_name_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/jquery']->get_acf_time_picker_field( null, 'Foo' );
+	}
+
+	/**
+	 * Test if getting acf date time picker with integer as $name value throw expected exception
+	 */
+	function test_get_acf_time_picker_field_throwing_exception_when_name_is_integer() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "integer".' );
+
+		$plugin['acf-helper/jquery']->get_acf_time_picker_field( 1, 'Foo' );
+	}
+
+	/**
+	 * Test if getting acf date time picker with null as $label value throw expected exception
+	 */
+	function test_get_acf_time_picker_field_throwing_exception_when_label_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/jquery']->get_acf_time_picker_field( 'bar', null );
+	}
+
+	/**
+	 * Test if getting acf date time picker with integer as $label value throw expected exception
+	 */
+	function test_get_acf_time_picker_field_throwing_exception_when_label_is_integer() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "double".' );
+
+		$plugin['acf-helper/jquery']->get_acf_time_picker_field( 'bar', 1.10 );
+	}
 }
