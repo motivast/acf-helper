4a8140b6d1f4351f03b909a1d6676721e1b4ab7a
Piotr Kierzniewski <p.kierzniewski@viewone.pl>
2017-08-04 20:20:42 +0200

    Add repeater field

--- inc/class-acf-helper-layout.php
+++ inc/class-acf-helper-layout.php
@@ -148,4 +148,72 @@ class Acf_Helper_Layout {
 
 		return $field;
 	}
+
+	/**
+	 * Get acf repeater field
+	 *
+	 * Helper function for retrieving acf repeater field.
+	 *
+	 * @param string $name Field name used as key and name.
+	 * @param string $label Field label displayed at the admin.
+	 * @param array  $sub_fields Repeatable field sub fields.
+	 * @param array  $args Additional arguments which extend defaults.
+	 *
+	 * @return array Acf repeater field
+	 *
+	 * @throws \InvalidArgumentException Throw exception if $name or $label are
+	 * 									 not string.
+	 */
+	public function get_acf_repeater_field( $name, $label, $sub_fields, $args = array() ) {
+
+		$field_defaults = array(
+			'key' => '',
+			'label' => '',
+			'name' => '',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'collapsed' => '',
+			'min' => 0,
+			'max' => 0,
+			'layout' => 'table',
+			'button_label' => '',
+			'sub_fields' => array(),
+		);
+
+		$field = wp_parse_args( $args, $field_defaults );
+
+		if ( ! is_string( $name ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $name parameter is not a type of string but "%s".', gettype( $name ) ) );
+		}
+
+		$field['key']  = 'field_' . $name;
+		$field['name'] = $name;
+
+		if ( ! is_string( $label ) ) {
+			throw new \InvalidArgumentException( sprintf( 'It looks like $label parameter is not a type of string but "%s".', gettype( $label ) ) );
+		}
+
+		$field['label'] = $label;
+
+		if ( ! is_array( $sub_fields ) || empty( $sub_fields ) ) {
+			throw new \InvalidArgumentException( 'It looks like $sub_fields parameter is not a type of array or is empty.' );
+		}
+
+		foreach ( $sub_fields as $sub_field_key => $sub_field_value ) {
+
+			$sub_fields[ $sub_field_key ]['key'] = sprintf( 'field_%s_%s', $name, $sub_fields[ $sub_field_key ]['name'] );
+			$sub_fields[ $sub_field_key ]['name'] = sprintf( '%s_%s', $name, $sub_fields[ $sub_field_key ]['name'] );
+		}
+
+		$field['sub_fields'] = $sub_fields;
+
+		return $field;
+	}
 }
--- tests/class-acf-helper-layout-test.php
+++ tests/class-acf-helper-layout-test.php
@@ -242,4 +242,224 @@ class Acf_Helper_Layout_Test extends WP_UnitTestCase {
 
 		$plugin['acf-helper/layout']->get_acf_tab_field( 'bar', 1.10 );
 	}
+
+	/**
+	 * Testing get_acf_repeater_field
+	 */
+
+	/**
+	 * Test if getting acf repeater field return expected results
+	 */
+	function test_get_acf_repeater_field() {
+
+		$plugin = acf_helper();
+
+		$sub_fields = array(
+			$plugin['acf-helper/basic']->get_acf_text_field( 'first_name', 'First name' ),
+			$plugin['acf-helper/basic']->get_acf_text_field( 'last_name', 'Last name' ),
+		);
+
+		$expected_sub_fields = array(
+			array(
+				'key' => 'field_foo_first_name',
+				'label' => 'First name',
+				'name' => 'foo_first_name',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+			array(
+				'key' => 'field_foo_last_name',
+				'label' => 'Last name',
+				'name' => 'foo_last_name',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+		);
+
+		$expected = array(
+			'key' => 'field_foo',
+			'label' => 'Foo',
+			'name' => 'foo',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'collapsed' => '',
+			'min' => 0,
+			'max' => 0,
+			'layout' => 'table',
+			'button_label' => '',
+			'sub_fields' => $expected_sub_fields,
+		);
+
+		$expected_filled = array(
+			'key' => 'field_foo',
+			'label' => 'Foo',
+			'name' => 'foo',
+			'type' => 'repeater',
+			'instructions' => 'Add persons to list',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'repeater',
+				'id' => 'bar_repeater',
+			),
+			'collapsed' => 'field_first_name',
+			'min' => 5,
+			'max' => 10,
+			'layout' => 'table',
+			'button_label' => 'Add person',
+			'sub_fields' => $expected_sub_fields,
+		);
+
+		$field 		  = $plugin['acf-helper/layout']->get_acf_repeater_field( 'foo', 'Foo', $sub_fields );
+		$field_filled = $plugin['acf-helper/layout']->get_acf_repeater_field( 'foo', 'Foo', $sub_fields, array(
+			'instructions' => 'Add persons to list',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50%',
+				'class' => 'repeater',
+				'id' => 'bar_repeater',
+			),
+			'collapsed' => 'field_first_name',
+			'min' => 5,
+			'max' => 10,
+			'layout' => 'table',
+			'button_label' => 'Add person',
+		) );
+
+		$this->assertEquals( $field, $expected );
+		$this->assertEquals( $field_filled, $expected_filled );
+	}
+
+	/**
+	 * Test if getting acf repeater with null as $name value throw expected exception
+	 */
+	function test_get_acf_repeater_field_throwing_exception_when_name_is_null() {
+
+		$plugin = acf_helper();
+
+		$sub_fields = array(
+			$plugin['acf-helper/basic']->get_acf_text_field( 'first_name', 'First name' ),
+			$plugin['acf-helper/basic']->get_acf_text_field( 'last_name', 'Last name' ),
+		);
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/layout']->get_acf_repeater_field( null, 'Foo', $sub_fields );
+	}
+
+	/**
+	 * Test if getting acf repeater with integer as $name value throw expected exception
+	 */
+	function test_get_acf_repeater_field_throwing_exception_when_name_is_integer() {
+
+		$plugin = acf_helper();
+
+		$sub_fields = array(
+			$plugin['acf-helper/basic']->get_acf_text_field( 'first_name', 'First name' ),
+			$plugin['acf-helper/basic']->get_acf_text_field( 'last_name', 'Last name' ),
+		);
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $name parameter is not a type of string but "integer".' );
+
+		$plugin['acf-helper/layout']->get_acf_repeater_field( 1, 'Foo', $sub_fields );
+	}
+
+	/**
+	 * Test if getting acf repeater with null as $label value throw expected exception
+	 */
+	function test_get_acf_repeater_field_throwing_exception_when_label_is_null() {
+
+		$plugin = acf_helper();
+
+		$sub_fields = array(
+			$plugin['acf-helper/basic']->get_acf_text_field( 'first_name', 'First name' ),
+			$plugin['acf-helper/basic']->get_acf_text_field( 'last_name', 'Last name' ),
+		);
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "NULL".' );
+
+		$plugin['acf-helper/layout']->get_acf_repeater_field( 'bar', null, $sub_fields );
+	}
+
+	/**
+	 * Test if getting acf repeater with integer as $label value throw expected exception
+	 */
+	function test_get_acf_repeater_field_throwing_exception_when_label_is_integer() {
+
+		$plugin = acf_helper();
+
+		$sub_fields = array(
+			$plugin['acf-helper/basic']->get_acf_text_field( 'first_name', 'First name' ),
+			$plugin['acf-helper/basic']->get_acf_text_field( 'last_name', 'Last name' ),
+		);
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $label parameter is not a type of string but "double".' );
+
+		$plugin['acf-helper/layout']->get_acf_repeater_field( 'bar', 1.10, $sub_fields );
+	}
+
+	/**
+	 * Test if getting acf repeater with null as $sub_fields value throw expected exception
+	 */
+	function test_get_acf_repeater_field_throwing_exception_when_sub_fields_is_null() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $sub_fields parameter is not a type of array or is empty.' );
+
+		$plugin['acf-helper/layout']->get_acf_repeater_field( 'foo', 'Foo', null );
+	}
+
+	/**
+	 * Test if getting acf repeater with empty array as $sub_fields value throw expected exception
+	 */
+	function test_get_acf_repeater_field_throwing_exception_when_sub_fields_is_empty_array() {
+
+		$plugin = acf_helper();
+
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'It looks like $sub_fields parameter is not a type of array or is empty.' );
+
+		$plugin['acf-helper/layout']->get_acf_repeater_field( 'bar', 'Bar', array() );
+	}
 }
